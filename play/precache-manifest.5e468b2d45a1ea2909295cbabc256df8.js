283e5e0c52ebbc3006d5e95d7ad8c46c33f6ba95
magicknight <magicknight@users.noreply.github.com>
2022-06-20 11:51:44 +0000

    deploy: magicknight/imec.ai@59c1c527a229f638817c0c857abfc3e120868b81

---
+++ play/precache-manifest.5e468b2d45a1ea2909295cbabc256df8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877ec1c8ef51f4ccff8a6e6c784d8e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde7cd407e00ca425477",
+    "url": "/static/css/main.92e41723.chunk.css"
+  },
+  {
+    "revision": "3679dda33264abc5edba",
+    "url": "/static/js/2.a070d964.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.a070d964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde7cd407e00ca425477",
+    "url": "/static/js/main.cfaac9f0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.cfaac9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3322f1781cd3a1258c3",
+    "url": "/static/js/runtime-main.3c444491.js"
+  }
+]);
--- play/service-worker.js
+++ play/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/play/precache-manifest.d562f9554ecbe6e42569557f26f54a2c.js"
+  "/precache-manifest.5e468b2d45a1ea2909295cbabc256df8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/play/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
