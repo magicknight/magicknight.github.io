7d8eb9a50b7fedd28a890078e44d6574b1e96303
magicknight <magicknight@users.noreply.github.com>
2022-06-21 08:59:46 +0000

    deploy: magicknight/imec.ai@32c4694d12d2b4e694482b99c62db8ed8fa381d5

---
+++ demo/precache-manifest.21bab5631288d932b5bb3a20e29e9bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18cbc7baff81625746b7429ca7389e9",
+    "url": "/play/index.html"
+  },
+  {
+    "revision": "940d767fdc8a5d5b0bf7",
+    "url": "/play/static/css/main.92e41723.chunk.css"
+  },
+  {
+    "revision": "3679dda33264abc5edba",
+    "url": "/play/static/js/2.a070d964.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/play/static/js/2.a070d964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940d767fdc8a5d5b0bf7",
+    "url": "/play/static/js/main.7abfc319.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/play/static/js/main.7abfc319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a3289ebc79285716bc",
+    "url": "/play/static/js/runtime-main.83b0dc1a.js"
+  }
+]);
---
+++ demo/precache-manifest.8f908c696519c3b943404732849db247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf66f8ca95fec3104ade877ded2a0cf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ed187c3a69e4eec5e3f",
+    "url": "/static/css/main.92e41723.chunk.css"
+  },
+  {
+    "revision": "3679dda33264abc5edba",
+    "url": "/static/js/2.a070d964.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.a070d964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed187c3a69e4eec5e3f",
+    "url": "/static/js/main.320bde0d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.320bde0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3322f1781cd3a1258c3",
+    "url": "/static/js/runtime-main.3c444491.js"
+  }
+]);
